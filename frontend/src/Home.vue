03276c1f970493a5684a3274d91331bf00578e29
rmn <rmswms@yandex.ru>
2024-08-06 17:28:48 +0300

    text div center

--- frontend/src/Home.vue
+++ frontend/src/Home.vue
@@ -9,7 +9,7 @@
               :width="250"/>
           </v-row>
           <v-row class="d-flex justify-center">
-            <div class="text-h1 font-weight-bold" style="width:60%;">С Днём Рождения, Валерий Олегович!</div>
+            <div class="text-h1 font-weight-bold" style="width:45%;">С Днём Рождения, Валерий Олегович!</div>
           </v-row>
           <v-row>
               <Vue3Lottie
