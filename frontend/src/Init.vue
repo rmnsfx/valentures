88a76eb285ae5beb33cac79966c3afee20a3999c
rmn <rmswms@yandex.ru>
2024-08-10 16:25:28 +0300

    fix ggl tag

--- frontend/src/Init.vue
+++ frontend/src/Init.vue
@@ -1,15 +1,8 @@
 <template>
 </template>
   
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-1YCM18H098"></script>
   <script>
   
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-1YCM18H098');
-
-
     export default {
       data: () => ({
   
