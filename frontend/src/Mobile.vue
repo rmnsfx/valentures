11ec22dd7f6575facb2373a268c4ac8c90ffc893
rmn <rmswms@yandex.ru>
2024-09-23 20:15:19 +0300

    v-row class

--- frontend/src/Mobile.vue
+++ frontend/src/Mobile.vue
@@ -1,17 +1,15 @@
 <template>
     <v-app id="inspire">
       <v-main class="bg-green-lighten-3">
-        <v-container fluid class="fill-height">
-          <v-row>
+        <v-container class="d-flex flex-column flex-nowrap">
+          <v-row class="flex-1-1-100 mb-10">
             <Vue3Lottie
               animationLink="https://lottie.host/b14150ff-70bd-47e3-9e37-41e94c5008b0/BkXe6swJiG.json"
               :height="200"
               :width="200"/>
           </v-row>
-          <v-row>
-            <v-col class="d-flex justify-center">
-              <div class="text-h2 font-weight-bold" style="width:100%;">С ДР БРО!</div>
-            </v-col>
+          <v-row class="text-center">
+            <div class="text-h1 font-weight-bold" style="width:100%">С ДР БРО!</div>
           </v-row>
           <v-row>
               <Vue3Lottie
